fc994b7efc231f57c2afcaad170f6335a1f2ea35
Efrain Vacas Medina <103590736+efrvame@users.noreply.github.com>
2023-08-01 19:33:26 -0500

    Primer diseño
    Estructura de la web y diseño inicial en HTML y CSS.

---
+++ index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://recipes-3cc20-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const recipesDB = ref(database, "recipes")
+
