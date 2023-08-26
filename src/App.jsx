3b687c0f74163ded2e2155d81e5873ff2929fad6
Efrain Vacas <efrvame@gmail.com>
2023-08-26 18:24:51 -0500

    Set up the project and crate both Navbar.jsx and Body.jsx components

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Body from "./components/Body";
+import Navbar from "./components/Navbar";
+
+import "./styles/index.css";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+            <Body />
+        </>
+    );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+function Navbar() {
+    return (
+        <header>
+            <nav className="navbar">
+                <h1>Tu Recetario</h1>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
